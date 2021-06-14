e7c92b9e912c95eb260c03cffd4ac429af9f32fd
OakBricks <oakbricks@gmail.com>
2021-06-13 19:20:42 -0500

    prepare

--- src/main/java/org/oakbricks/oakores/OakOres.java
+++ src/main/java/org/oakbricks/oakores/OakOres.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package org.oakbricks.oakores;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,8 +7,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = OakOres.MODID, name = OakOres.NAME, version = OakOres.VERSION)
+public class OakOres
 {
     public static final String MODID = "examplemod";
     public static final String NAME = "Example Mod";
